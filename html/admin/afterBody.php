f7dd06c3638b0a0dda83968729bfe7d9dac945d8
paweenwich <paweenwich@yahoo.com>
2017-09-13 12:31:07 +0700

    kwang

---
+++ html/admin/afterBody.php
@@ -0,0 +1,8 @@
+<!-- afterBody.php start -->
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KWJ66T"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager -->
+<!-- End Google Tag Manager (noscript) -->
+<!-- afterBody.php end -->
+
