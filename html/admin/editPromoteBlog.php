bc609c358592fb1436bc6a230d7e1df6d61255f9
daver <daver@mindfireinc.com>
2017-09-16 23:33:22 -0400

    Fixed up message

--- html/admin/editPromoteBlog.php
+++ html/admin/editPromoteBlog.php
@@ -106,7 +106,7 @@
 																		<div class="form-group">
 																			<label class="col-sm-2 control-label">URL Parameters (optional)</label>
 																			<div class="col-sm-10">
-																				<input class="form-control" name="URL-BLOG-POST-UTM" placeholder="utm_medium=email&utm_source=Blog&utm_campaign=My+Blog&utm_term=Read" type="text" ng-model="campaign['URL-BLOG-POST-UTM']"><span class="help-block m-b-none">URL parameters you specify here are appended to your Blog Post URL when sent via your emails. This spot is good for UTM and other tracking parameters.</span>
+																				<input class="form-control" name="URL-BLOG-POST-UTM" placeholder="utm_medium=email&utm_source=Blog&utm_campaign=My+Blog&utm_term=Read" type="text" ng-model="campaign['URL-BLOG-POST-UTM']"><span class="help-block m-b-none">URL parameters you specify here (like UTM tags used by Google Analytics) are appended to your Blog Post URL.</span>
 																			</div>
 																		</div>
 																		<div class="hr-line-dashed"></div>
