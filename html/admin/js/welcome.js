c9dcf1ee7716c99a10213e5f3a8035ed704824e5
daver <daver@mindfireinc.com>
2017-09-20 14:02:53 -0400

    [Welcome] Hide table of Campaigns if User's first time and no campaigns available #17

--- html/admin/js/welcome.js
+++ html/admin/js/welcome.js
@@ -2,6 +2,12 @@ var myApp = angular.module('myApp', ['angularMoment']);
 myApp.controller('myCtrl', function($scope, $http) {
     $scope.Reset = function() {
         $scope.campaignlist = angular.copy($scope.master);
+        // Added by Dave on 9/20/17 so that UI can know whether or not to display table of campaigns.
+        if ($scope.campaignlist.campaigns.length > 0) {
+        $scope.numberOfCampaigns = 1;
+        } else {
+        $scope.numberOfCampaigns = 0;
+        }
     };
     $scope.Load = function() {
         //$http.get("/couchdb/" + dbName + '/campaignlist?' + new Date().toString()).then(function(response) {
--- html/admin/welcome.php
+++ html/admin/welcome.php
@@ -3,25 +3,27 @@
     session_start();
     include 'global.php';
     require_once('loginCredentials.php');
-?><!DOCTYPE html>
+?>
+	<!DOCTYPE html>
 	<html ng-app="myApp">
 
 	<head>
 		<?php include "header.php"; ?>
+		<script src="js/welcome.js"></script>
 	</head>
 
 
 	<body class="">
-        <?php include "afterBody.php"; ?>
+		<?php include "afterBody.php"; ?>
 		<div id="wrapper">
 			<!-- left wrapper -->
-            <?php include 'leftWrapper.php'; ?>
+			<?php include 'leftWrapper.php'; ?>
 			<!-- /end left wrapper -->
 			<div id="page-wrapper" class="gray-bg">
 				<div class="row border-bottom">
 					<nav class="navbar navbar-static-top  " role="navigation" style="margin-bottom: 0">
 						<!-- top wrapper -->
-                        <?php include 'topWrapper.php'; ?>
+						<?php include 'topWrapper.php'; ?>
 						<!-- / top wrapper -->
 					</nav>
 				</div>
@@ -40,19 +42,21 @@
 						</div>
 					</div>
 				</div>
-				<div class="row" ng-controller="myCtrl" id="myCtrl">
-					<div class="col-lg-12">
-						<div class="wrapper wrapper-content animated fadeInUp">
-							<div class="ibox">
-								<div class="ibox-title">
-									<h5><i class="fa fa-paper-plane-o" style="color:orange"></i> Your Most Recent Campaigns</h5>
-									<!--
+				<div ng-controller="myCtrl" ng-cloak>
+					<!-- Some strange bug with empty arrays required me to use 1 or 0 to decide whether or not to display the table [Dave] -->
+					<div class="row" id="myCtrl" ng-if="numberOfCampaigns===1">
+						<div class="col-lg-12">
+							<div class="wrapper wrapper-content">
+								<div class="ibox">
+									<div class="ibox-title">
+										<h5><i class="fa fa-paper-plane-o" style="color:orange"></i> Your Most Recent Campaigns</h5>
+										<!--
 													<div class="ibox-tools">
 	                              <a class="btn btn-primary btn-xs" href=""><i aria-hidden="true" class="fa fa-external-link"></i> SEE ALL MY CAMPAIGNS</a>
                             </div>
 													-->
-								</div>
-								<!--
+									</div>
+									<!--
 											<div class="ibox-content">
                             <div class="row m-b-sm m-t-sm">
                                 <div class="col-md-6">
@@ -65,10 +69,10 @@
                                 </div>
                             </div>
 											-->
-								<div class="ibox-content">
+									<div class="ibox-content">
 
 
-									<!--<div class="row m-b-sm m-t-sm">
+										<!--<div class="row m-b-sm m-t-sm">
                                 <div class="col-md-1">
                                     <button type="button" id="loading-example-btn" class="btn btn-white btn-sm" ><i class="fa fa-refresh"></i> Refresh</button>
                                 </div>
@@ -78,61 +82,85 @@
                                 </div>
                             </div>-->
 
-									<div class="project-list">
+										<div class="project-list">
 
-										<table class="table table-hover">
-											<tbody>
-												<thead>
-													<tr>
-														<th>Status</th>
-														<th>Campaign Name</th>
-														<th>Reach (People)</th>
-														<th>Results</th>
-														<th>&nbsp;</th>
-														<th>&nbsp;</th>
-													</tr>
-												</thead>
-												<tr ng-repeat="item in campaignlist.campaigns | orderBy:'-lastEditDate' | filter:searchText | limitTo:3" ng-cloak ng-click="goToLink(item)">
-													<td class="project-status">
-														<span class="badge badge-published"><span class="badge badge-published"><i class="fa fa-rss"></i> {{item.status=='Edit' ? 'LIVE' : 'DRAFT'}}</span></span>
-													</td>
-													<td class="project-title">
-														<strong>{{item.campaignName}}</strong> <small>({{item.campaignType=='PromoteBlog' ? 'Promote a Blog Post' : 'Promote an eBook'}})</small>
-														<br/>
-														<!-- Roll back to this <span am-time-ago="message.time"></span> once timestamp issue is resolved -->
-														<small>Modified {{item.lastEditDate | amDateFormat:'MMMM Do YYYY, h:mm a'}}
+											<table class="table table-hover">
+												<tbody>
+													<thead>
+														<tr>
+															<th>Status</th>
+															<th>Campaign Name</th>
+															<th>Reach (People)</th>
+															<th>Results</th>
+															<th>&nbsp;</th>
+															<th>&nbsp;</th>
+														</tr>
+													</thead>
+													<tr ng-repeat="item in campaignlist.campaigns | orderBy:'-lastEditDate' | filter:searchText | limitTo:3" ng-cloak ng-click="goToLink(item)">
+														<td class="project-status">
+															<span class="badge badge-published"><span class="badge badge-published"><i class="fa fa-rss"></i> {{item.status=='Edit' ? 'LIVE' : 'DRAFT'}}</span></span>
+														</td>
+														<td class="project-title">
+															<strong>{{item.campaignName}}</strong> <small>({{item.campaignType=='PromoteBlog' ? 'Promote a Blog Post' : 'Promote an eBook'}})</small>
+															<br/>
+															<!-- Roll back to this <span am-time-ago="message.time"></span> once timestamp issue is resolved -->
+															<small>Modified {{item.lastEditDate | amDateFormat:'MMMM Do YYYY, h:mm a'}}
                                                         (<span am-time-ago="item.lastEditDate"></span>)</small>
-													</td>
+														</td>
 
-													<td class="project-reach">
-														<medium>{{Random(10000)|number}}</medium><br><small class="text-muted">&nbsp;</small>
-													</td>
-													<td class="project-completion">
-														<medium>{{Random(1000)}}</medium><br><small class="text-muted"><i class="fa fa-bullhorn"></i> Clicks to Blog Post</small>
-													</td>
-													<td class="project-completion">
-														<medium>{{Random(10)}}%</medium><br><small class="text-muted">Click-thru Rate</small>
-													</td>
-													<td class="project-actions">
-														<!--<a href="edit{{item.campaignType}}.php?campaign_id={{item.campaignID}}" class="btn btn-white btn-sm" data-toggle="tooltip" title="Copy feature coming soon."><i class="fa fa-clone" style="color:green"></i> Copy </a>-->
-														<a href="edit{{item.campaignType}}.php?campaign_id={{item.campaignID}}" class="btn btn-white btn-sm"><i class="fa fa-pencil" style="color:green"></i> Edit </a>
-													</td>
-												</tr>
-											</tbody>
-										</table>
-									</div>
-									<div class="text-center">
-										<a class="btn btn-success btn-xs" href="myCampaigns.php"><i aria-hidden="true" class="fa fa-external-link"></i> SEE ALL MY CAMPAIGNS</a>
+														<td class="project-reach">
+															<medium>{{Random(10000)|number}}</medium><br><small class="text-muted">&nbsp;</small>
+														</td>
+														<td class="project-completion">
+															<medium>{{Random(1000)}}</medium><br><small class="text-muted"><i class="fa fa-bullhorn"></i> Clicks to Blog Post</small>
+														</td>
+														<td class="project-completion">
+															<medium>{{Random(10)}}%</medium><br><small class="text-muted">Click-thru Rate</small>
+														</td>
+														<td class="project-actions">
+															<!--<a href="edit{{item.campaignType}}.php?campaign_id={{item.campaignID}}" class="btn btn-white btn-sm" data-toggle="tooltip" title="Copy feature coming soon."><i class="fa fa-clone" style="color:green"></i> Copy </a>-->
+															<a href="edit{{item.campaignType}}.php?campaign_id={{item.campaignID}}" class="btn btn-white btn-sm"><i class="fa fa-pencil" style="color:green"></i> Edit </a>
+														</td>
+													</tr>
+												</tbody>
+											</table>
+										</div>
+										<div class="text-center">
+											<a class="btn btn-success btn-xs" href="myCampaigns.php"><i aria-hidden="true" class="fa fa-external-link"></i> SEE ALL MY CAMPAIGNS</a>
+										</div>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
+					<div class="row" id="myCtrl" ng-if="numberOfCampaigns===0">
+						<div class="col-lg-12">
+							<div class="wrapper wrapper-content">
+								<div class="ibox">
+									<div class="ibox-title">
+										<h5><i class="fa fa-paper-plane-o" style="color:orange"></i> Hi <?php echo $USERNAME;?>, welcome to Da Vinci.</h5>
+									</div>
+										<div class="ibox-content">
+											<p>I'm going to help you generate more leads and sales from your prospects and customers.  Here are a few tips to get you started:</p>
+											<ol>
+												
+											<li>Click any of the navigation items on the left and take a quick look at the various options.</li>
+											<li>Create a quick sample campaign to get a feel for what's going on here.</li>
+											<li>[Note for Kushal] Please suggest appropriate content based on your experience</li>
+											</ol>
+
+											<p>If you need any help, use the chat box (found on every page), email us a <a href="mailto:support@mindfireinc.com">support@mindfireinc.com</mailto:support@mindfireinc.com> or call us at (877) 560-3473 and press option 2 from the main menu.</p>
+										</div>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
+
 				<!--/ content -->
 				<div class="footer">
 					<!-- footer -->
-                    <?php include 'footer.php'; ?>
+					<?php include 'footer.php'; ?>
 					<!-- / footer -->
 				</div>
 			</div>
@@ -158,11 +186,10 @@
 		<script src="https://cdn.jsdelivr.net/angular.moment/1.0.1/angular-moment.min.js"></script>
 
 		<!-- Page-Level Scripts -->
-        <script>
-            // welcome.js need this
-            var dbName = "<?php echo $dbName; ?>";
-        </script>
-		<script src="js/welcome.js"></script>
+		<script>
+			// welcome.js need this
+			var dbName = "<?php echo $dbName; ?>";
+		</script>
 	</body>
 
 	</html>
