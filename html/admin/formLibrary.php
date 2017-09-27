d97e15c98a36f83b90cd4af1d6bb3084683af995
AumpaiFung <aumpai@mindfiremail.info>
2017-09-27 15:47:48 +0700

    copy and edit Btn nobr

--- html/admin/formLibrary.php
+++ html/admin/formLibrary.php
@@ -82,11 +82,11 @@
                                                 <medium>{{item.submission}}</medium><br><small class="text-muted">people submitted this form</small>
                                         </td>
 
-                                        <td class="project-actions">
-                                            <!-- <a href="formEditorCopy.php?fID={{item.formID}}" class="btn btn-white btn-sm" ><i class="fa fa-clone" style="color:green"></i> Copy </a> -->
-											
+                                        <td class="project-actions">                                         
+											<nobr>
                                             <a href="#" class="btn btn-white btn-sm" ng-click="DuplicateFormClick(item.formID,item.formName)"><i class="fa fa-clone" style="color:green"></i> Copy </a>
                                             <a href="formEditor.php?fID={{item.formID}}" class="btn btn-white btn-sm"><i class="fa fa-pencil" style="color:green"></i> Edit </a>
+											</nobr>
                                         </td>
                                         
                                     </tr>                                   
