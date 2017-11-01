02a01855e4b0755accd0a8937ab0aa2b94da8fdc
daver <daver@mindfireinc.com>
2017-11-01 17:18:24 -0400

    Formatted numbers #198

--- html/admin/dv_report_mail.php
+++ html/admin/dv_report_mail.php
@@ -11,18 +11,18 @@
 		<div class="row">
 			<div class="col-xs-2"><small class="stats-label"><i aria-hidden="true" class="fa" style="color:green"></i>Email #</small></div>
 			<div class="col-xs-2"><small class="stats-label"><i aria-hidden="true" class="fa fa-users" style="color:green"></i> Targeted People</small></div>
-			<div class="col-xs-2"><small class="stats-label"><i aria-hidden="true" class="fa fa-envelope-open-o" style="color:blue"></i>	Delivered</small></div>
+			<div class="col-xs-2"><small class="stats-label"><i aria-hidden="true" class="fa fa-envelope-o" style="color:blue"></i>	Delivered</small></div>
 			<div class="col-xs-2"><small class="stats-label"><i aria-hidden="true" class="fa fa-envelope-open-o" style="color:blue"></i>	Opens</small></div>
 			<div class="col-xs-2"><small class="stats-label"><i aria-hidden="true" class="fa fa-mouse-pointer" style="color:purple"></i> Clicks to Blog Post</small></div>
 			<div class="col-xs-2"><small class="stats-label"><i aria-hidden="true" class="fa fa-times-circle" style="color:red"></i> Unsubscribes</small></div>
 		</div>
 		<div class="row" ng-repeat="item in campaign.report">
-			<div class="col-xs-2"><h2>{{item.emailName}}</h2></div>
-			<div class="col-xs-2"><h2>{{item.Sent}}</h2></div>
-			<div class="col-xs-2"><h2>{{item.Delivered}}</h2></div>
-			<div class="col-xs-2"><h2>{{item.Opened}}</h2></div>
-			<div class="col-xs-2"><h2>{{item.Clicked}}</h2></div>
-			<div class="col-xs-2"><h2>{{item.Unsubscribed}}</h2></div>
+			<div class="col-xs-2"><h2>{{item.emailName }}</h2></div>
+			<div class="col-xs-2"><h2>{{item.Sent | number}}</h2></div>
+			<div class="col-xs-2"><h2>{{item.Delivered | number}}</h2></div>
+			<div class="col-xs-2"><h2>{{item.Opened | number}}</h2></div>
+			<div class="col-xs-2"><h2>{{item.Clicked | number}}</h2></div>
+			<div class="col-xs-2"><h2>{{item.Unsubscribed | number}}</h2></div>
 		</div>
 	</div>
 </div>
