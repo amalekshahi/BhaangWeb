1f3b5f6df0ca3d69a56cc1111010ffd887de0de1
daver <daver@mindfireinc.com>
2017-11-02 13:20:14 -0400

    Removed collape link from performance table #205

--- html/admin/dv_report_mail.php
+++ html/admin/dv_report_mail.php
@@ -4,7 +4,7 @@
 		<h5><i aria-hidden="true" class="fa fa-bar-chart" style="color:black"></i> Performance Snapshot</h5>
 		<div class="ibox-tools">
 			<button class="btn btn-white btn-xs" ng-click="ViewReport()"><i aria-hidden="true" class="fa fa-external-link" style="color:blue"></i> VIEW MORE PERFORMANCE REPORTS</button>
-			<a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+			<!--<a class="collapse-link"><i class="fa fa-chevron-up"></i></a>-->
 		</div>
 	</div>
 	<div class="ibox-content" id="reportDiv">
