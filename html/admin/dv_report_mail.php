786cff73e4d391582c7202c46a2bcbae42eac71d
daver <daver@mindfireinc.com>
2017-11-02 13:18:43 -0400

    Fixed formatting and div width for conversions #205

--- html/admin/dv_report_mail.php
+++ html/admin/dv_report_mail.php
@@ -9,13 +9,12 @@
 	</div>
 	<div class="ibox-content" id="reportDiv">
 		<div class="row">
-			<div class="col-xs-1"><small class="stats-label"><i aria-hidden="true" class="fa" style="color:green"></i></small></div>
 			<div class="col-xs-2"><small class="stats-label"><i aria-hidden="true" class="fa fa-users" style="color:green"></i> Targeted People</small></div>
 			<div class="col-xs-2"><small class="stats-label"><i aria-hidden="true" class="fa fa-envelope-o" style="color:blue"></i>	Delivered</small></div>
 			<div class="col-xs-2"><small class="stats-label"><i aria-hidden="true" class="fa fa-envelope-open-o" style="color:blue"></i>	Opens</small></div>
 			<div class="col-xs-2"><small class="stats-label"><i aria-hidden="true" class="fa fa-mouse-pointer" style="color:purple"></i> Clicks to Blog Post</small></div>
 			<div class="col-xs-2"><small class="stats-label"><i aria-hidden="true" class="fa fa-times-circle" style="color:red"></i> Unsubscribes</small></div>
-			<div class="col-xs-1"><small class="stats-label"><i aria-hidden="true" class="fa fa-check-square" style="color:blue"></i> Conversions</small></div>
+			<div class="col-xs-2"><small class="stats-label"><i aria-hidden="true" class="fa fa-dot-circle-o" style="color:blue"></i> Conversions</small></div>
 		</div>
 		<!-- <div class="row" ng-repeat="item in campaign.report">
 			<div class="col-xs-1"><h3>{{item.emailName }}</h3></div>
@@ -27,13 +26,12 @@
 			<div class="col-xs-1"><h3>{{item.Conversions | number}} ({{item.ConversionsRate | number:0}}%)</h3></div>
 		</div> -->
 		<div class="row">
-			<div class="col-xs-1"><h3></h3></div>
-			<div class="col-xs-2"><h3>{{campaign.reportSumarySent | number}}</h3></div>
-			<div class="col-xs-2"><h3>{{campaign.reportSumaryDelivered | number}} ({{campaign.reportSumaryDeliveredRate | number:0}}%)</h3></div>
-			<div class="col-xs-2"><h3>{{campaign.reportSumaryOpened | number}} ({{campaign.reportSumaryOpenedRate | number:0}}%)</h3></div>
-			<div class="col-xs-2"><h3>{{campaign.reportSumaryClicked | number}} ({{campaign.reportSumaryClickedRate | number:0}}%)</h3></div>
-			<div class="col-xs-2"><h3>{{campaign.reportSumaryUnsubscribed | number}} ({{campaign.reportSumaryUnsubscribedRate | number:0}}%)</h3></div>
-			<div class="col-xs-1"><h3>{{campaign.reportSumaryConversions | number}} ({{campaign.reportSumaryConversionsRate | number:0}}%)</h3></div>
+			<div class="col-xs-2"><h2>{{campaign.reportSumarySent | number}}</h2></div>
+			<div class="col-xs-2"><h2>{{campaign.reportSumaryDelivered | number}} ({{campaign.reportSumaryDeliveredRate | number:0}}%)</h2></div>
+			<div class="col-xs-2"><h2>{{campaign.reportSumaryOpened | number}} ({{campaign.reportSumaryOpenedRate | number:0}}%)</h2></div>
+			<div class="col-xs-2"><h2>{{campaign.reportSumaryClicked | number}} ({{campaign.reportSumaryClickedRate | number:0}}%)</h2></div>
+			<div class="col-xs-2"><h2>{{campaign.reportSumaryUnsubscribed | number}} ({{campaign.reportSumaryUnsubscribedRate | number:0}}%)</h2></div>
+			<div class="col-xs-2"><h2>{{campaign.reportSumaryConversions | number}} ({{campaign.reportSumaryConversionsRate | number:0}}%)</h2></div>
 		</div>
 	</div>
 </div>
